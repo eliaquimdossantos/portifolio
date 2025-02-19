63ae48576cd2d1173e123918587eaf3a8b7b0e3e
Eliaquim dos Santos <eliaquimdossantos@gmail.com>
2025-02-18 23:28:55 -0300

    feat: adicionar estrutura inicial

---
+++ src/app.tsx
@@ -0,0 +1,12 @@
+import { useState } from 'preact/hooks'
+import preactLogo from './assets/preact.svg'
+import viteLogo from '/vite.svg'
+import './app.css'
+
+export function App() {
+    return (
+    <>
+      <h1 className="text-6xl">Teste</h1>
+    </>
+  )
+}
